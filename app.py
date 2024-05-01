76a2e4318d681b97287a03a312240ce2ac122360
Federico Gerardi <38018471+iGieri@users.noreply.github.com>
2024-05-01 14:59:53 +0200

    Team Details

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ def player_career(player_id):
     career = nba.PlayerCareerStats(player_id=player_id)
     return career.get_json()
 
+# Team info
+@app.route('/api/teams/<team_id>', methods=['GET'])
+def team_details(team_id):
+    team = nba.TeamDetails(team_id=team_id)
+    return team.get_json()
 
 if __name__ == '__main__':
     app.run("localhost", 5000, debug=True)
--- test.py
+++ test.py
@@ -12,11 +12,27 @@ for player in players:
     response = request('GET', url)
     data = json.loads(response.text)
     if int(data['parameters']['PlayerID']) == int(player):
-        print(f"Player {player} career stats: PASSED")
+        print(f"Player {player} career stats: \033[92mPASSED\033[0m")
         passed += 1
     else:
-        print(f"Player {player} career stats: FAILED")
+        print(f"Player {player} career stats: \033[91mFAILED\033[0m")
 
 print(f"Player career test: {passed}/{len(players)} passed")
 print("------END TESTING PLAYER CAREER------")
 
+print("------TESTING TEAM DETAILS------")
+
+teams = ['1610612737', '1610612738', '1610612747', '1610612744', '1610612745']
+passed = 0
+for team in teams:
+    url = f'http://localhost:5000/api/teams/{team}'
+    response = request('GET', url)
+    data = json.loads(response.text)
+    if int(data['parameters']['TeamID']) == int(team):
+        print(f"Team {team} career stats: \033[92mPASSED\033[0m")
+        passed += 1
+    else:
+        print(f"Team {team} career stats: \033[91mFAILED\033[0m")
+
+print(f"Player career test: {passed}/{len(players)} passed")
+print("------END TESTING PLAYER CAREER------")
